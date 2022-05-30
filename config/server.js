ed58f1ca94cfa131eaef3689a14ddba79a20de83
mri118 <mrityunjaysinha0118@gmail.com>
2022-05-30 22:25:12 +0530

    commit

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['RjRb69/cBKkHYG+Wmq9l4w==','EJ00yJma0Io18IX7GOuzSA==,DdQz+u7+2ONE2o4SW5oXJw==','w7EN8CZknU8zivLowa/U1w=='])
   },
 });
